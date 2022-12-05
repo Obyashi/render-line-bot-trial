c2bdbad63b1d396decc169d324805bc6a3c7b0e0
Obyashi <117147995+Obyashi@users.noreply.github.com>
2022-12-05 11:06:21 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,13 @@
-import sys
+from websocket_server import WebsocketServer
 
-data = sys.stdin.readline()
+def new_client(client, server):
+    print ("new_client:", client['address'])
 
-print(data)
+def message_received(client, server, message):
+    print ("message_received:", message)
+    server.send_message(client, "hello from server")
+
+server = WebsocketServer(port=5001, host='127.0.0.1')
+server.set_fn_new_client(new_client)
+server.set_fn_message_received(message_received)
+server.run_forever()
